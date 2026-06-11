c06c3fdbc6c80ab690a83eb267e2bf69dc92344c
JC <julian.clemens@knowunity.com>
2026-06-11 13:34:18 +0200

    Polyfill: map global to window for react-native-web Animated

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
+// Polyfill: react-native-web's Animated uses global.cancelAnimationFrame which
+// doesn't exist in browsers. Map global to window.
+if (typeof window !== 'undefined' && typeof window.global === 'undefined') {
+  window.global = window;
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
